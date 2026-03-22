53750b030332f7136307c041eccca08417a01261
Shiropan <shiropan>
2026-03-22 09:20:53 +0900

    PrismaClientのインスタンス化条件を変更し、常にグローバルに保持するように修正

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,6 +7,4 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+globalForPrisma.prisma = prisma;
